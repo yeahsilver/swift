1dd458284af9a505f123b3a0d98f60e851f03049
YeeunHeo <gjdpdms2005@gmail.com>
2021-01-05 12:09:22 +0900

    Add Notification

---
+++ Notification/Notification/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Notification
+//
+//  Created by 허예은 on 2021/01/05.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func setReminder(_ sender: Any){
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Reminder"
+        content.body = "This is a location notification"
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 20, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+        
+        center.add(request){
+            (error) in
+            print("Error = \(error?.localizedDescription ?? "error local notification")")
+        }
+    }
+}
+
